6b4954c994d3274bc433840662faf9fa9c64c024
Simon Arsenault <simarsenault@gmail.com>
2018-07-29 16:57:15 -0400

    Project structure

---
+++ ScriptSDK.SantiagoUO.RaillessLumberjacking/Program.cs
@@ -0,0 +1,17 @@
+﻿using StealthAPI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScriptSDK.SantiagoUO.RaillessLumberjacking
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+        }
+    }
+}
