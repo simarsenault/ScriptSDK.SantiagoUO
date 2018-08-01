af5d48007f0ef978f5301f4f19ae5908459f42b9
Simon Arsenault <simarsenault@gmail.com>
2018-07-31 21:54:27 -0400

    Functionnal railless lumberjacking

--- ScriptSDK.SantiagoUO.RaillessLumberjacking/RaillessLumberjacking.cs
+++ ScriptSDK.SantiagoUO.RaillessLumberjacking/RaillessLumberjacking.cs
@@ -12,7 +12,8 @@ namespace ScriptSDK.SantiagoUO.RaillessLumberjacking
 {
     public class RaillessLumberjacking
     {
-        private static readonly int TILE_SCAN_DISTANCE = 25;
+        private static readonly int TILE_SCAN_DISTANCE = 75;
+        private static readonly int LUMBERJACKING_HIT_TIMEOUT = 8000;
 
         public void Start()
         {
@@ -20,7 +21,8 @@ namespace ScriptSDK.SantiagoUO.RaillessLumberjacking
          
             while (tiles.Count > 0)
             {
-                var nearestTreeTile = tiles.OrderBy(tile => Math.Sqrt(Math.Pow((tile.X - PlayerMobile.GetPlayer().Location.X), 2) + Math.Pow((tile.Y - PlayerMobile.GetPlayer().Location.Y), 2))).First();
+                Data.Point3D playerLocation = PlayerMobile.GetPlayer().Location;
+                var nearestTreeTile = tiles.OrderBy(tile => Math.Sqrt(Math.Pow((tile.X - playerLocation.X), 2) + Math.Pow((tile.Y - playerLocation.Y), 2))).First();
 
                 MovingHelper.GetMovingHelper().newMoveXY(nearestTreeTile.X, nearestTreeTile.Y, true, 1, true);
                 
@@ -28,14 +30,41 @@ namespace ScriptSDK.SantiagoUO.RaillessLumberjacking
                 if (hatchet.Count == 0)
                     return;
 
-                hatchet.First().DoubleClick();
-                TargetHelper.GetTarget().WaitForTarget(10000);
-                TargetHelper.GetTarget().TargetTo(nearestTreeTile.Tile, new Data.Point3D(nearestTreeTile.X, nearestTreeTile.Y, nearestTreeTile.Z));
-
-                Thread.Sleep(8000); // TODO scan journal
-
-                tiles.Remove(nearestTreeTile);
+                if (ChopTree(hatchet.First(), nearestTreeTile) == ChopTreeResult.DONE)
+                    tiles.Remove(nearestTreeTile);
             }
+        }
+
+        private ChopTreeResult ChopTree(Item hatchet, StealthAPI.StaticItemRealXY tile)
+        {
+            hatchet.DoubleClick();
+            TargetHelper.GetTarget().WaitForTarget(10000);
+
+            DateTime dateTime = DateTime.Now;
+            DateTime maxDateTime = dateTime.AddMilliseconds(LUMBERJACKING_HIT_TIMEOUT);
+
+            TargetHelper.GetTarget().TargetTo(tile.Tile, new Data.Point3D(tile.X, tile.Y, tile.Z));
+
+            while (DateTime.Now < maxDateTime)
+            {
+                if (StealthAPI.Stealth.Client.InJournalBetweenTimes("You hack at the tree for a while, but fail to produce any useable wood", dateTime, DateTime.Now) >= 0)
+                    return ChopTreeResult.CONTINUE;
+
+                if (StealthAPI.Stealth.Client.InJournalBetweenTimes("You put the logs in your pack", dateTime, DateTime.Now) >= 0)
+                    return ChopTreeResult.CONTINUE;
+
+                if (StealthAPI.Stealth.Client.InJournalBetweenTimes("There is nothing here to chop", dateTime, DateTime.Now) >= 0)
+                    return ChopTreeResult.DONE;
+
+                Thread.Sleep(50);
+            }
+
+            return ChopTreeResult.DONE;
+        }
+
+        private enum ChopTreeResult
+        {
+            CONTINUE, DONE
         }
     }
 }
