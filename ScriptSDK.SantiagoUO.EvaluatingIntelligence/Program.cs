b06c94fd6e47b10dd6b9e02f0603652c8d211311
Simon Arsenault <simarsenault@gmail.com>
2018-08-02 09:45:24 -0400

    Evaluation Intelligence trainer

---
+++ ScriptSDK.SantiagoUO.EvaluatingIntelligence/Program.cs
@@ -0,0 +1,36 @@
+﻿using ScriptSDK.Mobiles;
+using ScriptSDK.SantiagoUO.Utilities;
+using ScriptSDK.SantiagoUO.Utilities.SkillGainTracker;
+using StealthAPI;
+
+namespace ScriptSDK.SantiagoUO.EvaluatingIntelligence
+{
+    class Program
+    {
+        private static readonly double MAXIMUM_SKILL_VALUE = 100;
+
+        static void Main(string[] args)
+        {
+            SkillGainTracker skillGainTracker = new SkillGainTracker(Skill.EvaluateIntelligence, new DiscordSkillChangeEventHandler());
+            skillGainTracker.Start();
+
+            while (StealthAPI.Stealth.Client.GetSkillValue(Skill.EvaluateIntelligence) < MAXIMUM_SKILL_VALUE)
+            {
+                var human = ObjetsFinder.Find<Mobile>(EasyUOItem.MOBILE_HUMANS, 2).Find(_human => _human.Serial.Value != PlayerMobile.GetPlayer().Serial.Value);
+                if (human == null)
+                {
+                    StealthAPI.Stealth.Client.Wait(1000);
+
+                    continue;
+                }
+
+                StealthAPI.Stealth.Client.UseSkill(Skill.EvaluateIntelligence);
+                StealthAPI.Stealth.Client.WaitForTarget(5000);
+                StealthAPI.Stealth.Client.TargetToObject(human.Serial.Value);
+                StealthAPI.Stealth.Client.Wait(2000);
+            }
+
+            skillGainTracker.Stop();
+        }
+    }
+}
--- ScriptSDK.SantiagoUO.Utilities/EasyUOItem.cs
+++ ScriptSDK.SantiagoUO.Utilities/EasyUOItem.cs
@@ -6,6 +6,7 @@
         public static readonly string MOBILE_HUMAN_FEMALE = "HS";
 
         public static readonly string[] HATCHETS = new string[] { "ASF", "FSF" };
+        public static readonly string[] PICKAXES = new string[] { "NPF", "QPF" };
         public static readonly string[] MOBILE_HUMANS = new string[] { MOBILE_HUMAN_MALE, MOBILE_HUMAN_FEMALE };
     }
 }
